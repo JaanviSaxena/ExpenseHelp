a9a6bfa746b1a9f734dfd7ef3aee2ea749aab71f
JaanviSaxena <150419377+JaanviSaxena@users.noreply.github.com>
2023-11-13 12:51:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//---------------------------Home Page--------------------------------
+ 
+//Login-register dialog box
+ const wrapper= document.querySelector('.wrapper');
+ const loginLink = document.querySelector('.login-link');
+ const registerLink = document.querySelector('.register-link');
+
+ registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+ });
+
+ loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+ });
