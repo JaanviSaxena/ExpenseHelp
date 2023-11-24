d4b02a0b6a1b406dd30f30be0f9a9a21e961fc00
JaanviSaxena <150419377+JaanviSaxena@users.noreply.github.com>
2023-11-24 22:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,28 @@
  loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
  });
+
+ //------------------------------Expense Report-------------------------------------------------------
+
+ function setReminder() {
+   const taskName = document.getElementById('taskname').value;
+   const dueDate = new Date(document.getElementById('duedate').value);
+   const currentDate = new Date();
+ 
+   if (dueDate < currentDate) {
+     document.getElementById('reminder').innerHTML = 'Please select a future date.';
+   } else {
+     const timeDiff = dueDate - currentDate;
+     const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+ 
+     document.getElementById('reminder').innerHTML = `Task "${taskName}" is due in ${daysRemaining} days.`;
+   }
+ }
+
+ var food = document.getElementById('food').value;;
+ var travel = document.getElementById('travel').value;
+ var bills = document.getElementById('bills').value;
+ var misc = document.getElementById('misc').value;
+
+ var total = food+travel+bills+misc;
+ var total = document.getElementById('total').value;
